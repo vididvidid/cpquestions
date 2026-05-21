be462be8e99d9fca3332b2a0a5814c0bbb47229b
vididvidid <vididvidid@gmail.com>
2026-05-21 14:05:40 +0000

    lcm(1.. 42) does not exceed 10^18

--- 1855B.c
+++ 1855B.c
@@ -12,15 +12,11 @@ void solve()
 {
     int n;
     scanf("%llu",&n);
-    int cn = 1, ans = 1;
-    for (int i = 2; i * i <= n; i++)
+    int ans = 0;
+    for (int i = 1; ;i++)
     {
-        if ((n%i==0) && (n%(i-1)==0)) cn++;
-        else cn = 1;
-        if (cn > ans)
-        {
-            ans = cn;
-        }
+        if (n % i == 0) ans++;
+        else break;
     }
     printf("%llu\n",ans);
 }
