45ab7f428eacf2442f7b3eeeb2599aa2518ce080
vididvidid <vididvidid@gmail.com>
2026-06-22 12:59:58 +0530

    again forgot to correctly give the end of the sort function

--- cquest/1807G1.c
+++ cquest/1807G1.c
@@ -41,7 +41,7 @@ void solve()
     int arr[n];
     for (int i = 0; i < n; i++) scanf("%d", &arr[i]);
 
-    sort(arr, arr+n);
+    sort(arr, arr+n - 1);
 
     int sum = arr[0];
     if (arr[0] != 1)
