10088e4d44e6d630ca08858627deb0109d7d296f
vididvidid <vididvidid@gmail.com>
2026-07-02 15:46:06 +0530

    xor property always leads to even number of pairs

--- cquest/869A.c
+++ cquest/869A.c
@@ -14,50 +14,51 @@ void solve()
     for (int i = 0; i < n; i++) scanf("%d", &arr[i]);
     for (int i = 0; i < n; i++) scanf("%d", &prr[i]);
 
-    if (n  == 1)
-    {
-        if (arr[0] != 0 && arr[0] % 2 == 0) 
-        {
-            printf("Koyomi\n");
-        }
-        else {
-            printf("Karen\n");
-        }
-        return;
-    }
+/*     if (n  == 1) */
+/*     { */
+/*         if (arr[0] != 0 && arr[0] % 2 == 0) */ 
+/*         { */
+/*             printf("Koyomi\n"); */
+/*         } */
+/*         else { */
+/*             printf("Karen\n"); */
+/*         } */
+/*         return; */
+/*     } */
 
-    int ca = 0, cb = 0;
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            int num = arr[i] ^ prr[j];
-            for (int k = 0; k < n; k++)
-            {
-                if (num == arr[k])
-                {
-                    ca++;
-                    break;
-                }
-                if (num == prr[k])
-                {
-                    cb++;
-                    break;
-                }        
-            }
-        }
-    }
+/*     int ca = 0, cb = 0; */
+/*     for (int i = 0; i < n; i++) */
+/*     { */
+/*         for (int j = 0; j < n; j++) */
+/*         { */
+/*             int num = arr[i] ^ prr[j]; */
+/*             for (int k = 0; k < n; k++) */
+/*             { */
+/*                 if (num == arr[k]) */
+/*                 { */
+/*                     ca++; */
+/*                     break; */
+/*                 } */
+/*                 if (num == prr[k]) */
+/*                 { */
+/*                     cb++; */
+/*                     break; */
+/*                 } */        
+/*             } */
+/*         } */
+/*     } */
     
-    /* printf("%d %d\n",ca,cb); */
+/*     /1* printf("%d %d\n",ca,cb); *1/ */
    
-    if (ca != 0 && ca % 2 == 0)
-    {
-        printf("Koyomi\n");
-    }
-    if (cb != 0 && cb % 2 == 0)
-    {
-        printf("Karen\n");
-    }
+/*     if (ca != 0 && ca % 2 == 0) */
+/*     { */
+/*         printf("Koyomi\n"); */
+/*     } */
+/*     if (cb != 0 && cb % 2 == 0) */
+/*     { */
+/*         printf("Karen\n"); */
+/*     } */
+    printf("Karen\n");
 }
 
 int main()
