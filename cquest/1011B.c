4b7edb333a0067d1c144d025d50c394388b046c4
vididvidid <vididvidid@gmail.com>
2026-07-16 23:37:15 +0530

    reading it

---
+++ cquest/1011B.c
@@ -0,0 +1,17 @@
+/*
+ * File: 1011B.c
+ * Author: vididvidid 
+ * Created: 2026-07-16 23:20:30
+ */
+
+/*
+ * n people
+ * m daily food package -> ai food type
+ * each participant -> one food package -> each day -> same food eat
+ * different participant may eat different(same ) type of food
+ * find how many days can eat same food.. 
+ * 
+ * n < m -> 0
+ * n == m -> 1
+ * n > m :
+ *  
