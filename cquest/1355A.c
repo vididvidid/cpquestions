494e48deab1bc4f4f6403354fd65acab2bd68869
vididvidid <vididvidid@gmail.com>
2026-07-16 15:26:30 +0530

    very good pattern

--- cquest/1355A.c
+++ cquest/1355A.c
@@ -7,6 +7,12 @@
 /*
  * recurrence relation an+1 = an + min(an) + max(an)
  *
+ * observation that i forgot ..
+ * after sometime there will be min as 0 only.. so all it bcome 0 * max = 0
+ *
+ * here important point was we are adding something to original a and that is
+ * multiple of min * max so as min becoame 0 the a will never change it will
+ * stuck at the same number
  */
 
 #include <stdio.h>
@@ -42,9 +48,12 @@ void solve()
     for (int i = 2; i <= k; i++)
     {
         int s = mm(a);
-        int tmp = a;
-        /* printf("D: a = %llu mn = %llu mx = %llu\n",tmp, s % 10, s/10); */
-        a = tmp + ((s % 10) * (s / 10));
+        int mn = s % 10;
+        int mx = s / 10;
+        if (mn == 0)
+            break;
+        a += mn * mx;
+        
     }
 
     printf("%llu\n",a);
